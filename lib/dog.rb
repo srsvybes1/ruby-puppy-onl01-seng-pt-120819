382f32fc0cc80da8b9755b9084c252df607ef571
Cody Toney-Griffith <cody.toney@gmail.com>
2020-02-02 00:23:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,10 @@ class Dog
   attr_accessor :name, :save
   attr_reader :save
   @@all = []
-  def initialize(name,save)
+  def initialize(name, save)
   @name = name 
   @save = save
-  @@all << @save
+  @@all << @self
    end
   
   def self.all 
@@ -17,7 +17,7 @@ class Dog
   def self.print_all 
     @@all.map {|i| puts i.name}
     def save 
-     @@all << @save 
+     @@all << @self 
     end 
     end 
 end
